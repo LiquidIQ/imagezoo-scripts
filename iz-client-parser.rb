36a0613272d6f5228a27102f23db799eede366dc
Toph <topher@bythecode.agency>
2018-05-01 21:59:40 -0400

    customer output file created. See notes in kaizen

--- iz-client-parser.rb
+++ iz-client-parser.rb
@@ -8,34 +8,22 @@ fname = Array.new
 lname = Array.new
 email = Array.new
 company = Array.new
-address1 = Array.new
-address2 = Array.new
-city = Array.new
-province = Array.new
-province_code = Array.new
-country = Array.new
-country_code = Array.new
-zip = Array.new
 accepts_marketing = Array.new
 total_spent = Array.new
 total_orders = Array.new
 tags = Array.new
 note = Array.new
 tax_exempt = Array.new
+shopify_write = Array.new
+tags = Array.new
 
 vLast = csvClients.length - 1
-csvClients.reverse.each_with_index do |row, i|
+
+csvClients.each_with_index do |row, i|
     nFname = 19
     nLname = 18
     nEmail = 14
     nCompany = 1
-    nAddress1 = 2
-    nAddress2 = 3
-    nCity = 5
-    nProvince = 6
-    # nProvince_code = 7
-    nCountry = 8
-    nZip = 7
     nAccepts_marketing = 15
 
     unless i == vLast 
@@ -44,26 +32,50 @@ csvClients.reverse.each_with_index do |row, i|
         lname << row[nLname]
         email << row[nEmail]
         company << row[nCompany]
-        address1 << row[nAddress1]
-        address2 << row[nAddress2]
-        city << row[nCity]
-        province << row[nProvince]
-        country << row[nCountry]
-        zip << row[nZip]
-        accepts_marketing << row[nAccepts_marketing]
-
-    end
-end
-csvSales[4].each_with_index do |fuckkkkkk|
-    if i > 0
-        clientId.each_with_index do |id, j|
-            if id == row[4]
-                puts "match row #{j} #{id}"
-            end
+        tags << ""
+        if row[nAccepts_marketing] == "1" 
+            accepts_marketing << "yes"
+        else 
+            accepts_marketing << "no"
         end
     end
 end
 
+csvSales.each_with_index do |row, i|
+    clientId.each_with_index do |id, j|
+        if row[4] == id
+            if tags[j] == "" 
+                tags[j] << "#{row[0]}"
+            else 
+                unless tags[j].include? row[0]
+                    tags[j] << ",#{row[0]}"
+                end
+            end
+        end
+    end
+end
+ 
+# tags.each_with_index do |tag, i|
+#     puts "#{company[i]}: #{tag}" if tag != "" 
+# end
+fname.each_with_index do |name, i|
 
-
-# puts clientIds
+end
+CSV.open("customer_template.csv", "a+") do |csv|
+    fname.each_with_index do |name, i|
+        unless tags[i] == "" 
+            if email[i] == ""
+                csvClients.each_with_index do |row, j|
+                    if company[i] == row[1] && row[14] != ""
+                        email[i] == row[14]
+                    end
+                end
+            end
+            unless company[i] == "Corbis"
+                unless company[i] == "Getty Images"
+                    csv << [name, lname[i], email[i], company[i], "", "", "", "", "", "", "", "", "",accepts_marketing[i], "", "", tags[i], "", "no"]
+                end
+            end
+        end
+    end
+end
