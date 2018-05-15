0526e768b94c7ba92eba7381d6db57e23b419b4d
Toph <topher@bythecode.agency>
2018-05-15 12:19:52 -0400

    ready to run. sending to lil mac

--- shopify_product_uploader.rb
+++ shopify_product_uploader.rb
@@ -1,10 +1,14 @@
 require 'csv'
+require 'rubygems'
 require 'shopify_api'
-require 'date'
+require 'pry'
 
+
+WATERMARK_URL = "http://watermarklibs.s3-website-us-east-1.amazonaws.com/"  
 API_KEY = "5be1e793270a36242c1368d6de5c3a9d"
 PASSWORD = "ddcafc1072286dfb03dba080f7f5083d"
 SHOP_NAME = "imagezoo"
+CYCLE = 0.5
 shop_url = "https://#{API_KEY}:#{PASSWORD}@#{SHOP_NAME}.myshopify.com/admin"
 ShopifyAPI::Base.site = shop_url
 
@@ -17,26 +21,66 @@ SUPPLIER_COL = 1
 DATE_COL = 2
 TITLE_COL = 3
 COLLECTION_COL = 26
+CSV.open("uploaded-products.csv", "ab")
 
 def build_artist_hash(artists)
     csv_artists = CSV.read('iz-artists.csv')
-
     csv_artists.each do |row|
         artists << { row[ID_COL] => row[NAME_COL] }
     end
-
-    puts artists
-
     return artists
 end
 
+
+def prod_already_uploaded(product_id)
+    uploaded_products = CSV.read("uploaded-products.csv")
+    id_list = Array.new
+    uploaded_products.each do |row|
+        id_list << row[0]
+    end
+    return id_list.index {|e| e.include? product_id} == nil ? false : true
+end
+
+def prod_not_in_aws(product_id)
+    aws_products = CSV.read("all.csv")
+    id_list = Array.new
+    aws_products.each do |row|
+        id_list << row[0]
+    end
+    return id_list.index {|e| e.include? product_id} == nil ? true : false
+end
+
 def send_prods_to_shopify(artists, products)
-    product_images = get_product_images("all.txt")
     csv_products = CSV.read('iz-images.csv')
-    puts product_images
+    product_count = csv_products.size
+
+    start_time = Time.now
+    products_remaining = product_count
+
     csv_products.each do |row|
+        # avoiding duplication of server work 
+        if prod_already_uploaded(row[ID_COL]) || prod_not_in_aws(row[ID_COL])
+            products_remaining -= 1
+            puts products_remaining
+            next
+        end
+
+        # convert vendor initials to full name
         vendor_name = artist_id_to_name(artists, row[SUPPLIER_COL])
-        # puts vendor_name
+
+        # pausing to keep our shopify api_call bucket full
+        stop_time = Time.now
+        processing_duration = stop_time - start_time
+        puts "The processing lasted #{processing_duration.to_i} seconds."
+        puts "#{products_remaining} Remaining"
+        wait_time = (CYCLE - processing_duration).ceil
+        puts "We have to wait #{wait_time} seconds then we will resume."
+        sleep wait_time if wait_time > 0
+        start_time = Time.now
+
+        # we know that this exists if we're here
+        product_watermark = get_product_image(row[ID_COL])
+
         product = ShopifyAPI::Product.new({
             title: row[TITLE_COL], 
             vendor: vendor_name,
@@ -53,18 +97,17 @@ def send_prods_to_shopify(artists, products)
                     price: "1.00",
                     sku: "#{row[ID_COL]}-Web"
                 }
+            ],
+            images: [
+                src: product_watermark
             ]
         })
-        begin
-            product.save
-            CSV.open("uploaded-products.csv", "ab") do |csv|
-                csv << [product.sku]
-            end
-            puts Time.now
-        rescue ActiveResource::ClientError => e
-            puts e.message
-            sleep(10)
-            retry
+
+        product.save
+        products_remaining -= 1
+
+        CSV.open("uploaded-products.csv", "ab") do |csv|
+            csv << [product.sku]
         end
     end
 end
@@ -77,14 +120,14 @@ def artist_id_to_name(artists, id)
     end
 end
 
-def get_product_images(filename)
-    txt_products = File.open(filename, "r")
-    products = Array.new
+def get_product_image(image_id)
+    csv_products = CSV.open("all.csv")
+    products = Hash.new
 
-    txt_products.each_line do |line|
-        products << [line[0], line[1]]
+    csv_products.each do |row|
+        products.store(row[0], row[1])
     end
-    return products
+    return "#{WATERMARK_URL}#{products.dig(image_id)}"
 end
 
 artists = build_artist_hash(artists)
